028dec44480a229a6ed8a35cd107a9e961a07cb1
Salovar Shaikh <salovar.shaikh@nitorinfotech.com>
2026-02-08 23:52:13 +0530

    chore: finalize backend env configuration for production

--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/TODO')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
